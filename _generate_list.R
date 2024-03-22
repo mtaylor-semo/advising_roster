1ed740ac11f33486728272025ba9633d8b076ca8
Michael S. Taylor <mtaylor@semo.edu>
2024-03-22 08:09:57 -0500

    Use file_name var for all uses of file name.

--- _generate_list.R
+++ _generate_list.R
@@ -1,9 +1,15 @@
+# Be sure to remove grad students from html roster. 
+# See readme.md file for details.
+
 file_name <- "roster_f2024"
 
 file.create("file_name.txt")
-cat("\"roster_f2024\"", file = "file_name.txt")
+#cat("\"roster_f2024\"", file = "file_name.txt")
+cat(file_name, file = "file_name.txt")
 
 rmarkdown::render("advisee_list.Rmd", clean = TRUE)
 
-file.rename("advisee_list.pdf", "roster_f2024.pdf")
+#file.rename("advisee_list.pdf", "roster_f2024.pdf")
+file.rename("advisee_list.pdf", paste0(file_name, ".pdf"))
+
 file.remove("file_name.txt")
